012b68fd589a9113bd63efbe24985bc63b3f5d74
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-09-11 23:08:48 +0900

    Create 3_1이_3개_이상_있는_위치.py

---
+++ Samsung/Simulation/dx_dy_technique/3_1이_3개_이상_있는_위치.py
@@ -0,0 +1,60 @@
+# 첫번째 풀이
+n = int(input())                                            # 격자 크기
+grid = [list(map(int, input().split())) for _ in range(n)]  # 격자
+
+# 상하좌우
+dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+# 범위 확인
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+ans = 0
+
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        for dx, dy in zip(dxs, dys):
+            nx, ny = i + dx, j + dy
+            if in_range(nx, ny) and grid[nx][ny] == 1:
+                cnt += 1
+        if cnt >= 3:
+            ans += 1
+
+print(ans)
+
+---------------------------------------------------------
+# 두번째 풀이
+# 변수 선언 및 입력
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dxs = [0, 1,  0, -1]
+dys = [1, 0, -1,  0]
+
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+def adjacent_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr[nx][ny] == 1:
+            cnt += 1
+    
+    return cnt
+
+
+# 각 칸을 탐색합니다.
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if adjacent_cnt(i, j) >= 3:
+            ans += 1
+    
+print(ans)
