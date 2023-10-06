075ca0a04c5ea7616a8dd3d08a590662625e3b3f
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-10-06 17:29:57 +0900

    Create 2_국영수_순이지.py

---
+++ Samsung/Simulation/Class_Sort/2_국영수_순이지.py
@@ -0,0 +1,32 @@
+# Class
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name, self.kor, self.eng, self.math = name, kor, eng, math
+
+
+n = int(input())
+students = []
+for _ in range(n):
+    name, k, e, m = input().split()
+    students.append(Student(name, int(k), int(e), int(m)))
+
+# 점수가 높은 학생부터 출력하므로 내림차순 정렬
+students.sort(key = lambda x : (-x.kor, -x.eng, -x.math))
+
+for student in students:
+    print(student.name, student.kor, student.eng, student.math)
+
+------------------------------------------------------------
+# Tuple
+n = int(input())
+students = []
+for _ in range(n):
+    name, kor, eng, math = input().split()
+    students.append((name, int(kor), int(eng), int(math)))
+
+# 점수가 높은 학생부터 출력하므로 내림차순 정렬
+# tuple 0: 이름, 1: 국어, 2: 영어, 3: 수학
+students.sort(key = lambda x : (-x[1], -x[2], -x[3]))
+
+for name, kor, eng, math in students:
+    print(name, kor, eng, math)
