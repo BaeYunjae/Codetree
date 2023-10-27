a77de3f1095c2f1c8a2eb791fc001871697b52f6
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2023-10-28 00:05:33 +0900

    Update 1_k개_중에_1개를_n번_뽑기.py

--- Samsung/Backtracking/1_k개_중에_1개를_n번_뽑기.py
+++ Samsung/Backtracking/1_k개_중에_1개를_n번_뽑기.py
@@ -1,3 +1,4 @@
+## 나의 코드
 k, n = map(int, input().split())
 
 answer = []
@@ -19,3 +20,29 @@ def choose(curr_num):
     return
 
 choose(1)
+
+--------------------------------
+## 정답 코드
+# 변수 선언 및 입력
+k, n = map(int, input().split())
+selected_nums = []
+
+# 선택된 원소들을 출력해준다.
+def print_permutation():
+    for num in selected_nums:
+        print(num, end=" ")
+    print()
+
+def find_permutations(cnt):
+    if cnt == n:
+        print_permutation()
+        return
+
+    # i부터 k까지의 각 숫자가 뽑혔을 때의 경우를 탐색한다.
+    for i in range(1, k + 1):
+        selected_nums.append(i)
+        find_permutations(cnt + 1)
+        selected_nums.pop()
+
+
+find_permutations(0)
